e085a492e6da8813ed4e70e6adbec8212d63dfee
André Junior <ajjunior33@gmail.com>
2020-02-16 14:33:29 -0300

    Updates

---
+++ precache-manifest.f793fb2408b1b5d30a7a5ec19e5b3791.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541b927f24597025394443ecd76753ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff4fd8c364f5e6705e0a",
+    "url": "/static/css/main.f3a3b5d7.chunk.css"
+  },
+  {
+    "revision": "bf4b7da5a7ba5a69996e",
+    "url": "/static/js/2.e396edc4.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.e396edc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff4fd8c364f5e6705e0a",
+    "url": "/static/js/main.49548e92.chunk.js"
+  },
+  {
+    "revision": "d7eb2bd9526174665150",
+    "url": "/static/js/runtime-main.e16f5dad.js"
+  }
+]);
